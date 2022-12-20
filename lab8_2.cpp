520eb90ab75a9deb9b76a365a44503557e221c85
TRex612 <poochit.moonkaew@gmail.com>
2022-12-20 15:41:45 +0700

    cm

--- lab8_2.cpp
+++ lab8_2.cpp
@@ -16,3 +16,36 @@ Fahsai: Next Monday....that is OK!!! I'm looking forward to watching Star Wars V
 Luke Skywalker: May the Force be with you krub
 Fahsai: 555+ see you Next Monday. Bye Bye \(^ ^)/
 */
+
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main(){
+    string nameuser_1 = "" , movie = "" , bduser = "" , seeu= "";
+    int iduser_1;
+    string x = "Fahsai";
+    cout << x << ": " << "Sawadee ka...Can you tell me your name?\n" ;
+    cout << "?????: ";
+    getline(cin,nameuser_1);
+    cout << x << ": " << "Wow!!! " << nameuser_1 << " is a really cool name.\n";
+    cout << x << ": " << "I think you are an Engineering student. What is your student ID?\n" ;
+    cout << nameuser_1 <<": ";
+    cin >> iduser_1 ;
+    int y ;
+    y = (iduser_1/10000000) -12 ;
+    cout << x << ": " << "I think you may be GEAR " << y << ". I have a free movie ticket for you.\n";
+    cout << x << ": " <<  "Let's go to the cinema together!!!\n";
+    cout << x << ": " <<  "What movie do you want to watch?\n";
+    cout << nameuser_1 <<": ";
+    cin.ignore();
+    getline(cin,movie);
+    cout << x << ": " << "So....which day are you free to go with me?\n";
+    cout << nameuser_1 <<": ";
+    getline(cin,bduser);
+    cout << x << ": " << bduser << "....that is OK!!! I'm looking forward to watching "<<movie<<" with you.\n";
+    cout << nameuser_1 <<": ";
+    getline(cin,seeu);
+    cout << x << ": " <<"555+ see you "<<bduser<<". Bye Bye \\(^ ^)/" ;
+
+}
